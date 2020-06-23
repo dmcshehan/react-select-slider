1e0723a4b267ce11d158b542add75e16d09fed4c
Shehan <dmcshehan@outlook.com>
2020-06-24 00:00:33 +0530

    change heights

--- src/index.js
+++ src/index.js
@@ -4,11 +4,7 @@ import "./index.css";
 
 import App from "./App/App";
 
-// We listen to the resize event
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 ReactDOM.render(<App />, document.getElementById("root"));
