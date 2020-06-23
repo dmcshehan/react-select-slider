4dc92ecb9aca7fecaf17b6ae2a8a7b1ac8530082
Shehan <dmcshehan@outlook.com>
2020-06-23 23:03:35 +0530

    replace .json with js

--- src/App/Slider/Slider.js
+++ src/App/Slider/Slider.js
@@ -3,7 +3,7 @@ import Slide from "./Slide/Slide";
 import SliderFooter from "./SliderFooter/SliderFooter";
 import styles from "./Slider.module.css";
 
-const sliderInfo = require("../../api/sliders.json");
+import sliderInfo from "../../api/sliders";
 
 const initialSelectedSlideOptions = {};
 
