88c88f524a355481d3390c3f151507c441479d72
Shehan <dmcshehan@outlook.com>
2020-06-23 23:35:25 +0530

    removed height

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import "./index.css";
 
 import App from "./App/App";
 
+// We listen to the resize event
+window.addEventListener("resize", () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
+
 ReactDOM.render(<App />, document.getElementById("root"));
