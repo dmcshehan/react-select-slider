b1a746cc363a484bdfde305217038f9518a77552
Shehan <dmcshehan@outlook.com>
2020-06-24 01:06:15 +0530

    add helper function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import fixHeight from "./helpers/fixHeight";
 import "./index.css";
 
 import App from "./App/App";
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+fixHeight();
+window.addEventListener("resize", fixHeight);
 
 ReactDOM.render(<App />, document.getElementById("root"));
